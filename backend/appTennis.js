190f7a3cedda53cf68a23988020fc4aeb80059b4
Fede Vergés <fedeverges@gmail.com>
2021-02-03 16:46:00 -0300

    Se agregan cambios para que el servidor funcione

--- backend/appTennis.js
+++ backend/appTennis.js
@@ -4,20 +4,23 @@ const   express = require('express'), // IMPORTAR express (framework de enrutami
         app = express(), // Inicializamos el servidor 
         api=require('./src');//Ruta donde esta la API 
         path = require('path')
+
 //Configuraciones del Server
 app.use(bodyParser.json());
 app.use(bodyParser.json({limit:'5mb'}));
 app.use('/api', api);
-app.use(express.static(path))
-app.engine('html', require('ejs').renderFile); //  esta es para que los archivos html sean renderizados por ejs.
-app.set('views', path.join(__dirname,'views')); // __dirname da la direccion absoluta de este archivo // le digo donde esta la carpeta views
+
+// app.use(express.static(path));
+// app.engine('html', require('ejs').renderFile); //  esta es para que los archivos html sean renderizados por ejs.
+// app.set('views', path.join(__dirname,'views')); // __dirname da la direccion absoluta de este archivo // le digo donde esta la carpeta views
 app.use(express.static(path.join(__dirname,'public')));
-app.set('view engine', 'ejs'); // Uso motor de la vista ejs. (mete js en html con sintaxis <%= %>)
+// app.set('view engine', 'ejs'); // Uso motor de la vista ejs. (mete js en html con sintaxis <%= %>)
 
 //Esto es para que imprima las peticiones cuando llegan en la consola (un middelware o algo asi)
 app.use(morgan('dev'));
 
 app.set('port',3000); //Puerto de donde va a correr 
+
 // Start the server
 app.listen(app.get('port'), () => {
     console.log('Servidor en puerto ' + app.get('port'));
